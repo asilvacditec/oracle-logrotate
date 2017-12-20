5ef0a601cd37d4fca3e57bf7d726d5d25c66404a
Aparecido Souza <asilva.cditec@gmail.com>
2017-12-19 21:33:55 -0300

    Created functions create_configfile and update_crontab

--- findlog.sh
+++ findlog.sh
@@ -19,41 +19,48 @@ function findlog() {
   find "${1}" -name "${2}" 2>/dev/null | grep "\.log"$
 }
 
-for x in ${DIRSTOSEARCH}; do
-  for y in ${LOGSTOSEARCH}; do
-        findlog "${x}" "${y}" >> "${FILECONFIG}"
-  done
-done
+function create_configfile() {
+	for x in ${DIRSTOSEARCH}; do
+	  for y in ${LOGSTOSEARCH}; do
+	        findlog "${x}" "${y}" >> "${FILECONFIG}"
+	  done
+	done
 
+	echo ""
+	echo ""
+	echo ""
+	echo "{" >> $FILECONFIG
+	echo "  weekly" >> $FILECONFIG
+	echo "  size 100M" >> $FILECONFIG
+	echo "  dateext" >> $FILECONFIG
+	echo "  create" >> $FILECONFIG
+	echo "  rotate 4" >> $FILECONFIG
+	echo "  copytruncate"  >> $FILECONFIG
+	echo "  missingok" >> $FILECONFIG
+	echo "  notifempty" >> $FILECONFIG
+	echo "  compress" >> $FILECONFIG
+	echo "}" >> $FILECONFIG
+}
 
-echo ""
-echo ""
-echo ""
-echo "{" >> $FILECONFIG
-echo "  weekly" >> $FILECONFIG
-echo "  size 100M" >> $FILECONFIG
-echo "  dateext" >> $FILECONFIG
-echo "  create" >> $FILECONFIG
-echo "  rotate 4" >> $FILECONFIG
-echo "  copytruncate"  >> $FILECONFIG
-echo "  missingok" >> $FILECONFIG
-echo "  notifempty" >> $FILECONFIG
-echo "  compress" >> $FILECONFIG
-echo "}" >> $FILECONFIG
+function update_crontab() {
+	echo "" > $DIR/crontab_logrotate.txt
+	echo "" >> $DIR/crontab_logrotate.txt
+	echo "# DXCDBA - logrotate Oracle Database and Grid Infraestructure - ${DATE_ID}" >> $DIR/crontab_logrotate.txt
+	echo "59 23 * * * /usr/sbin/logrotate -vf -s \
+	${DIR}/logrotate.status ${DIR}/logrotate.dba.dxc.conf 1> \
+	${DIR}/logrotate.log 2>&1" >> ${DIR}/crontab_logrotate.txt
 
-echo "" > $DIR/crontab_logrotate.txt
-echo "" >> $DIR/crontab_logrotate.txt
-echo "# DXCDBA - logrotate Oracle Database and Grid Infraestructure - ${DATE_ID}" >> $DIR/crontab_logrotate.txt
-echo "59 23 * * * /usr/sbin/logrotate -vf -s \
-${DIR}/logrotate.status ${DIR}/logrotate.dba.dxc.conf 1> \
-${DIR}/logrotate.log 2>&1" >> ${DIR}/crontab_logrotate.txt
+	crontab -l > ${DIR}/backup_crontab.txt.${DATE_ID}
 
-crontab -l > ${DIR}/backup_crontab.txt.${DATE_ID}
+	crontab -l > ${DIR}/crontab.tmp
 
-crontab -l > ${DIR}/crontab.tmp
+	cat ${DIR}/crontab_logrotate.txt >> ${DIR}/crontab.tmp
 
-cat ${DIR}/crontab_logrotate.txt >> ${DIR}/crontab.tmp
+	crontab crontab.tmp
 
-crontab crontab.tmp
+	crontab -l
+}
 
-crontab -l
+create_configfile
+
+update_crontab
